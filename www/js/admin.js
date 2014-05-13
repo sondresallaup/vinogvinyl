585ee82664fd90ee962ced68d6aec0ddd0e4a214
Sondre Sallaup <sondre.sallaup@gmail.com>
2014-05-13 15:51:44 +0200

    admin.js

---
+++ www/js/admin.js
@@ -0,0 +1,61 @@
+Parse.initialize("mO5SuziTjoOwT28NdwfTaBkEvGaS1aMIZF0s6GPW", "6uTteekIXC1kbX3RYUgYjTqtgjJlWe4AjeNhG5Ps");
+
+var Album = Parse.Object.extend("Album");
+var query = new Parse.Query(Album);
+query.notEqualTo("isPlayed",true);
+query.ascending("createdAt");
+
+var t=setInterval(printPlaylist,1000);
+
+function printPlaylist(){
+
+    query.find({
+        success: function(results){
+            var albumsString = "<ol>";
+            for(var i = 0; i < results.length; i++){
+                var album = results[i];
+                albumsString += '<li class="item">';
+                if(i == 0){
+                    albumsString += "<b>";
+                    $("div#footer").html('<button class="button button-block button-positive" onclick="nextAlbum();">Neste album</button>');
+                }
+                albumsString += album.get("albumName") + " - " + album.get("artistName") + "</li>";
+                
+                if(i == 0){
+                    albumsString += '</b>';   
+                }
+            }
+            
+            
+            $("div#nowplaying").html(albumsString);   
+        }
+    ,
+       error: function(results, error){
+        localStorage.setItem('albumAndArtist', 'redwine');
+        localStorage.setItem('album', 'redwine');
+         if(localStorage.getItem('albumAndArtist' != 'redwine')){
+                    location.reload();
+                }
+    }
+    }
+              
+              );
+    
+}
+
+function nextAlbum(){
+     query.find({
+        success: function(results){
+           var album = results[0];
+            album.save(null, {
+              success: function(album) {
+                album.set("isPlayed", true);
+                album.save();
+              }});
+    }
+    });
+    
+    
+    
+    
+}
